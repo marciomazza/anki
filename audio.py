ee0fec8e512c19b78e44702a09e4dc30888a14e3
Marcio Mazza <marciomazza@gmail.com>
2016-12-18 20:05:45 -0200

    Adjust

--- audio.py
+++ audio.py
@@ -9,7 +9,8 @@ canceled = []
 
 
 def download(note):
-    print(note['German'])
+    print('%s %s -> %s' % tuple(
+        note[k] for k in ('Noun_Article', 'German', 'English')))
     do_download(note, get_note_fields(note), 'de')
     if note['German_Audio']:
         print('  ', note['German_Audio'])
@@ -17,29 +18,32 @@ def download(note):
     else:
         canceled.append(note.id)
 
+
 def more(notes=None, limit=10):
     notes = notes or find('is:due')
     for note in [n for n in notes
                  if not n['German_Audio'] and n.id not in canceled][:limit]:
         download(note)
 
-new = [n for n in find('is:new') if any(c.due < 20000 for c in n.cards())]
+# new = [n for n in find('"deck:Duolingo German" is:new')
+#        if any(c.due < 20000 for c in n.cards())]
+new = find('"deck:Duolingo German" is:new')
 new = sorted(new, key=lambda n: min(c.due for c in n.cards()))
 
 
 canceled = [1460960126017,
- 1460960126613,
- 1460960126854,
- 1460960127358,
- 1460960127372,
- 1460960125913,
- 1460960125914,
- 1460960125915,
- 1460960125969,
- 1460960126005,
- 1460960126018,
- 1460960126019,
- 1460960126020,
- 1460960126239,
- 1460960126615,
- 1460960126739]
+            1460960126613,
+            1460960126854,
+            1460960127358,
+            1460960127372,
+            1460960125913,
+            1460960125914,
+            1460960125915,
+            1460960125969,
+            1460960126005,
+            1460960126018,
+            1460960126019,
+            1460960126020,
+            1460960126239,
+            1460960126615,
+            1460960126739]
